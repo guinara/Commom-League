70a0d03f6f114884f10d3c24fb242892535baed1
Juan R. Rodrigues <juancontato27@gmail.com>
2024-05-21 01:22:54 -0300

    1

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://ec2-18-230-65-128.sa-east-1.compute.amazonaws.com:8080/',
+    baseURL: 'https://ec2-18-230-65-128.sa-east-1.compute.amazonaws.com:8080/',
     headers: {
         Accept: 'application/json',
     }
