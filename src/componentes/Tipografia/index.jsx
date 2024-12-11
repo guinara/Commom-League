832a3e8829259daf0fc1e0c9e20c3e8b9aa3806b
Juan R. Rodrigues <juancontato27@gmail.com>
2024-12-10 21:52:36 -0300

    Correção

--- src/componentes/Tipografia/index.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled"
-
-export const Tipografia = ({ variante, componente, children }) => {
-    const tag = componentes[componente]
-    const ComponenteUtilizado = styled[tag]`${estilos[variante]}`
-    return (<ComponenteUtilizado>
-        {children}
-    </ComponenteUtilizado>)
-}
